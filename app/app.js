634b782fa6b309969abe704dcfc1d8f64851163a
Grabbe98 <53019193+Grabbe98@users.noreply.github.com>
2019-07-21 00:18:19 +0200

    changed login-site

--- app/app.js
+++ app/app.js
@@ -12,3 +12,47 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
 
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+// Login-Prozess
+function Login(){
+  var UserName = getUserName();
+  var Password = getPassword();
+  var correctLoginData = checkLoginData(UserName, Password);
+}
+
+function getUserName(){
+  // Zieht den UserNamen aus dem HTML Dokument
+  return document.querySelector("#UserName_input").value;
+}
+
+function getPassword(){
+  // Zieht das Passwort aus dem HTML Dokument
+  return document.querySelector("#Password_input").value;
+}
+
+function checkLoginData(UserName, Password){
+  // Checkt ob die LoginDaten im JSON sind
+  if(UserName == "Louis" && Password =="1234"){
+    alert("Login erfolgreich!");
+    return true;
+  }else{
+    showPopup();
+    return false;
+  }
+}
+
+function showPopup(){
+  // Zieht sich das PopUp
+  var popUpLogin = document.getElementById("popUpLogin");
+
+  // Zieht sich das (x) zum schließen
+  var span = document.getElementsByClassName("popup_close")[0];
+
+  // Macht das PopUp sichtbar
+  popUpLogin.style.display = "block";
+
+  // Wenn der Benutzer das (x) drückt wird das Pupup geschlossen
+  span.onclick = function() {
+    popUpLogin.style.display = "none";
+  }
+}
